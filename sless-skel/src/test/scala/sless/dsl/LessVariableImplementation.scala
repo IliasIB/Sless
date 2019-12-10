d12621d4bc21cfd926225787fd24895b37ed213c
Ilias Bachiri <ilias.bachiri@gmail.com>
2019-12-10 20:44:39 +0100

    Added missing LessVariable left out due to refactoring

--- sless-skel/src/test/scala/sless/dsl/LessVariableImplementation.scala
+++ sless-skel/src/test/scala/sless/dsl/LessVariableImplementation.scala
@@ -1,8 +1,17 @@
 package sless.dsl
 
+import sless.ast._
+
 object LessVariableImplementation {
   type DSL = PropertyDSL with SelectorDSL with ValueDSL with Compilable
-  val dsl: DSL = ???
+  val dsl: DSL = new CompilableHandler with PropertyHandler with ValueHandler with SelectorHandler {
+    override type Value = ValueAST
+    override type Selector = SelectorAST
+    override type Property = PropertyAST
+    override type Css = CssAST
+    override type Declaration = BaseDeclarationAST
+    override type Rule = BaseRuleAST
+  }
 
   import LessVariableImplementation.dsl._
 
@@ -12,7 +21,9 @@ object LessVariableImplementation {
     * @param color
     * @return
     */
-  def coloredBg(color: String): dsl.Declaration = ???
+  def coloredBg(color: String): dsl.Declaration = {
+    prop("background-color") := value(color)
+  }
 
   /**
     * Create a css sheet that colors an element with the given id in red
@@ -21,7 +32,13 @@ object LessVariableImplementation {
     * @param id
     * @return
     */
-  def colorNamedRed(id: String): dsl.Css = ???
+  def colorNamedRed(id: String): dsl.Css = {
+    css(
+      All.##(id) {
+        coloredBg("red")
+      }
+    )
+  }
 
   /**
     * Create a rule for the given element type that has an aspect ratio of 2/1
@@ -29,6 +46,11 @@ object LessVariableImplementation {
     * @param height
     * @return
     */
-  def doubledWidth(elementType: String, height: Int): dsl.Rule = ???
+  def doubledWidth(elementType: String, height: Int): dsl.Rule = {
+    tipe(elementType).apply(
+      prop("height") := value(height.toString),
+      prop("width") := value((height * 2).toString)
+    )
+  }
 
 }
